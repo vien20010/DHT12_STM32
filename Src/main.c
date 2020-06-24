b4ad93e4b8bc93aa93293f4453821a6b9bc369af
LV_Embedded <nguyenlamvien0110@gmail.com>
2020-06-24 15:13:40 +0700

    use only one call function to read data

--- Src/main.c
+++ Src/main.c
@@ -2,22 +2,27 @@
 #include "DHT12.h"
 #include "Systick.h"
 uint8_t temp=0,temp1=0,hum=0,hum1=0;
+uint8_t count=0;
+uint8_t buffer[4];
 int main()
 {
     
     
 		Systick_Configuration();
     I2C_Config();
-		//uint8_t buffer[5];
+		
     while (1)
     {
-        I2C_Read(I2Cxx,SLAVE_ADDRESS,0x00,&hum,1);
-        SystickDelay_ms(3);
-        I2C_Read(I2Cxx,SLAVE_ADDRESS,0x01,&hum1,1);
-        SystickDelay_ms(3);
-        I2C_Read(I2Cxx,SLAVE_ADDRESS,0x02,&temp,1);
-        SystickDelay_ms(3);
-        I2C_Read(I2Cxx,SLAVE_ADDRESS,0x03,&temp1,1);
-        SystickDelay_ms(3); 
+        I2C_Read(I2Cxx,SLAVE_ADDRESS,0x00,buffer,4);
+				SystickDelay_ms(1000);
+        // hum=I2C_Read(I2Cxx,SLAVE_ADDRESS,0x00,buffer,1);
+        // SystickDelay_ms(5);
+        // hum1=I2C_Read(I2Cxx,SLAVE_ADDRESS,0x01,buffer,1);
+        // SystickDelay_ms(5);
+        // temp=I2C_Read(I2Cxx,SLAVE_ADDRESS,0x02,buffer,1);
+        // SystickDelay_ms(5);
+        // temp1=I2C_Read(I2Cxx,SLAVE_ADDRESS,0x03,buffer,1);
+        // SystickDelay_ms(5); 
+				count++;
     }
 }
